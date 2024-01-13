ba71afc3f2616a36985a698845bbec6cefc6330e
bf93023 <149819880+bf93023@users.noreply.github.com>
2024-01-12 18:54:48 -0700

    Fixed Plot Error and updated formatting

--- RangePrediction.py
+++ RangePrediction.py
@@ -8,18 +8,18 @@ os.system("cls")  # Clearing the terminal for better visibility
 
 
 # -----------INPUTS-----------------
-lift_drag_ratios  = np.array([4, 5, 6, 7, 8])  # Lift/Drag
-cruise_mach_numbers  = np.linspace(2, 7, 10)  # Cruse Mach number
-total_ranges  = [3500, 4000, 4500, 5000, 5500]  # Total range in nmi
-earth_radius  = 20925641.8  # Radius of the earth in ft
-gravity_acceleration  = 32.174  # Acceleration due to gravity in ft/s^2
-takeoff_acceleration  = 0.3 * gravity_acceleration  # Take off Acceleration in ft/s^2
-climb_distance_arrays  = [None] * len(lift_drag_ratios)
-percentage_cruise_arrays  = [None] * len(lift_drag_ratios)
-total_time_arrays  = [None] * len(lift_drag_ratios)
-climb_distance_arrays_arrays  = []
-percentage_cruise_arrays_arrays  = []
-total_time_arrays_arrays  = []
+lift_drag_ratios = np.array([4, 5, 6, 7, 8])  # Lift/Drag
+cruise_mach_numbers = np.linspace(2, 7, 10)  # Cruse Mach number
+total_ranges = [3500, 4000, 4500, 5000, 5500]  # Total range in nmi
+earth_radius = 20925641.8  # Radius of the earth in ft
+gravity_acceleration = 32.174  # Acceleration due to gravity in ft/s^2
+takeoff_acceleration = 0.3 * gravity_acceleration  # Take off Acceleration in ft/s^2
+climb_distance_arrays = [None] * len(lift_drag_ratios)
+percentage_cruise_arrays = [None] * len(lift_drag_ratios)
+total_time_arrays = [None] * len(lift_drag_ratios)
+climb_distance_arrays_arrays = []
+percentage_cruise_arrays_arrays = []
+total_time_arrays_arrays = []
 
 
 # ----------------------TAKE OFF/THROTTLE UP---------------------------------------
@@ -94,7 +94,14 @@ def run_calculations(LoD, MachNum, TotRan, re, g, a_t):
         total_time_arrays_arrays.append(total_time_arrays.copy())
 
 
-run_calculations(lift_drag_ratios, cruise_mach_numbers, total_ranges, earth_radius, gravity_acceleration, takeoff_acceleration)
+run_calculations(
+    lift_drag_ratios,
+    cruise_mach_numbers,
+    total_ranges,
+    earth_radius,
+    gravity_acceleration,
+    takeoff_acceleration,
+)
 
 
 def plot_graphs(x_vals, y_vals, xlabel, ylabel, title, legend_labels):
@@ -109,13 +116,25 @@ def plot_graphs(x_vals, y_vals, xlabel, ylabel, title, legend_labels):
 
 
 # Generate s_L vs Mach plot
-plot_graphs(cruise_mach_numbers, climb_distance_arrays_arrays, "Mach", "$s_L$", "$s_L$ vs Mach", legend_labels=None)
+plot_graphs(
+    cruise_mach_numbers,
+    climb_distance_arrays_arrays[0],
+    "Mach",
+    "$s_L$",
+    "$s_L$ vs Mach",
+    legend_labels=None,
+)
 
 # Generate s_C/Range vs Mach plots
 for r in range(len(total_ranges)):
     title = f"$\\frac{{s_C}}{{\\text{{Range}}}}$ vs Mach: Range {total_ranges[r]} nmi"
     plot_graphs(
-        cruise_mach_numbers, percentage_cruise_arrays_arrays[r], "Mach", title, title, legend_labels=lift_drag_ratios
+        cruise_mach_numbers,
+        percentage_cruise_arrays_arrays[r],
+        "Mach",
+        title,
+        title,
+        legend_labels=lift_drag_ratios,
     )
 
 
